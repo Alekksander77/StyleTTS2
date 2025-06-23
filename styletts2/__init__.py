65529554b1f5320a165b96f3019cf10dc03bf429
Alekksander77 <alextemim1@gmail.com>
2025-06-23 18:55:27 +0300

    Add French language support

---
+++ styletts2/__init__.py
@@ -0,0 +1,13 @@
+"""StyleTTS2 package providing training utilities and models."""
+
+from . import models, meldataset, optimizers, utils, text_utils, losses
+
+__all__ = [
+    "models",
+    "meldataset",
+    "optimizers",
+    "utils",
+    "text_utils",
+    "losses",
+]
+
