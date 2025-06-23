94b2ee33835304ea8225a7579b3537e7d93dd8f5
Alekksander77 <alextemim1@gmail.com>
2025-06-23 19:04:21 +0300

    Export training and inference helpers

--- styletts2/__init__.py
+++ styletts2/__init__.py
@@ -1,6 +1,8 @@
 """StyleTTS2 package providing training utilities and models."""
 
 from . import models, meldataset, optimizers, utils, text_utils, losses
+from .inference import TTSModel, prepare_dataset
+from .train import first_stage, second_stage, finetune, finetune_accelerate
 
 __all__ = [
     "models",
@@ -9,5 +11,11 @@ __all__ = [
     "utils",
     "text_utils",
     "losses",
+    "TTSModel",
+    "prepare_dataset",
+    "first_stage",
+    "second_stage",
+    "finetune",
+    "finetune_accelerate",
 ]
 
